d179769d939c21cd80260e045418404306bfb3a8
saurav123anand <123sauravanand@gmail.com>
2022-12-27 12:50:16 +0530

    microservice incremental development like Api gateway implementation, config server implementation added

---
+++ jee/spring/boot/microservices/hotelms/userService/src/main/java/com/example/userService/controller/StaffController.java
@@ -0,0 +1,19 @@
+package com.example.userService.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/staffs")
+public class StaffController {
+    @GetMapping
+    public ResponseEntity<List<String>> getStaffs(){
+        return new ResponseEntity<>(Arrays.asList("Guchhu", "sweetu", "chudail"), HttpStatus.OK);
+    }
+}
