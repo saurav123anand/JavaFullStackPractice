1ef015b52b391dd04cc7ee38b07554cd73205fd7
saurav123anand <123sauravanand@gmail.com>
2022-12-29 11:33:10 +0530

    microservices with circuit breaker done, microservices completed

--- jee/spring/boot/microservices/hotelms/userService/src/main/java/com/example/userService/controller/UserController.java
+++ jee/spring/boot/microservices/hotelms/userService/src/main/java/com/example/userService/controller/UserController.java
@@ -2,10 +2,12 @@ package com.example.userService.controller;
 
 import com.example.userService.entities.User;
 import com.example.userService.service.IUserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -19,10 +21,22 @@ public class UserController {
     public User saveUser(@RequestBody User user){
         return service.saveUser(user);
     }
+
+    @CircuitBreaker(name = "ratingHotelBreaker",fallbackMethod = "ratingHotelFallback")
     @GetMapping("/findbyuserid/{userid}")
     public User findUserByUserId(@PathVariable("userid") String userId){
-        return service.findByUserId(userId);
+        User user= service.findByUserId(userId);
+        return user;
     }
+    public User ratingHotelFallback(String userId,Exception e){
+        User user=new User();
+        user.setUsername("dummy username");
+        user.setEmail("dummy@gmail.com");
+        user.setUserId("1234");
+        user.setRatings(new ArrayList<>());
+        return user;
+    }
+
     @GetMapping("/findall")
     public List<User> findAllUser(){
         return service.findAllUser();
