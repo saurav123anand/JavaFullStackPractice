0111270bd26cd75ed690941fa82509f5affbac0d
saurav123anand <123sauravanand@gmail.com>
2023-03-26 09:38:14 +0530

    Git and GitHub added, sql docs updated,query added

---
+++ jee/resources/demo.sql
@@ -0,0 +1,17 @@
+-- insert into student(id,name,sub) values(1,"Rohan","Mathematics");
+-- insert into student(id,name,sub) values(2,"Sohan","C++");
+-- insert into student(id,name,sub) values(3,"Kabir","Java");
+-- insert into student(id,name,sub) values(4,"Zoya","Mathematics");
+
+-- AND/OR
+-- select * from student where name="Rohan" and sub="Mathematics" 
+-- select * from student where name="Rohan" or sub="Mathematics" 
+
+-- SQL IN Clause
+-- select * from student where sub in('c++','Mathematics');
+
+-- SQL UPDATE Statement
+-- UPDATE Student set fees=10000 where id=1;
+
+-- SQL BETWEEN Clause
+-- select * from student where fees between 10000 and 150000
